2230c7439cfc8f0c8353841b5cb06a0f3911c70d
m1oojv <97022614+m1oojv@users.noreply.github.com>
2025-10-05 11:09:48 +0800

    chore: Update frontend config

--- jest.frontend.config.js
+++ jest.frontend.config.js
@@ -25,22 +25,24 @@ export default {
     "<rootDir>/client/src/components/*.test.js",
     "<rootDir>/client/src/context/cart.test.js",
     "<rootDir>/client/src/context/*.test.js",
-    "<rootDir>/client/src/components/Routes/Private.test.js"
+    "<rootDir>/client/src/components/Routes/Private.test.js",
   ],
 
   // jest code coverage
   collectCoverage: true,
   collectCoverageFrom: [
     "client/src/pages/**/**",
-    "client/src/components/**",
+    "client/src/components/**.js",
+    "client/src/components/Form/**",
+    "client/src/components/Routes/Private.js",
     "client/src/context/cart.js",
-    "client/src/context/auth.js"
+    "client/src/context/auth.js",
   ],
-    
+
   coverageThreshold: {
     global: {
-      lines: 10,
-      functions: 10,
+      lines: 50,
+      functions: 50,
     },
   },
   setupFilesAfterEnv: ["<rootDir>/client/src/setupTests.js"],
