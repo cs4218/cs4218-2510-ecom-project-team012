2bcf290d77f49d75735292f9731c42fb58158c2c
m1oojv <97022614+m1oojv@users.noreply.github.com>
2025-10-21 09:32:59 +0800

    Add category product ui test

--- tests/seed_data/categories.js
+++ tests/seed_data/categories.js
@@ -20,6 +20,13 @@ export const CATEGORIES = [
   name: "Clothing",
   slug: "clothing",
   __v: 0
+},
+
+{
+  _id: new mongoose.Types.ObjectId("66db427fdb0119d9234b27ea"),
+  name: "Furniture",
+  slug: "furniture",
+  __v: 0
 }
 ];
 
