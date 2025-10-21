8085573edd68d167628f48a77504738740c837f1
LHeng1 <limhengyi1@gmail.com>
2025-10-21 11:39:49 +0800

    add config folder integration test path

--- jest.backend.integration.config.js
+++ jest.backend.integration.config.js
@@ -16,6 +16,7 @@ export default {
     "<rootDir>/controllers/*.integration.test.js",
     "<rootDir>/helpers/*.integration.test.js",
     "<rootDir>/middlewares/*.integration.test.js",
+    "<rootDir>/config/*.integration.test.js"
   ],
 
   // jest code coverage
